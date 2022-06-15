b680302c3e2d9028cf3ac99afdb95f641eaf0fbd
Mike Melchior <mike.melchior@outlook.com>
2022-06-15 17:04:27 -0400

    early commit

--- src/index.js
+++ src/index.js
@@ -1,18 +1,91 @@
-import _ from 'lodash';
+import './style.css';
 
-function component() {
+
+function restaurantPage() {
     const element = document.createElement('div');
-    const btn = document.createElement('button');
+    element.setAttribute('id', 'content');
+
+    return element;
+};
+
+document.body.appendChild(restaurantPage());
+
+function header() {
+    const element = document.createElement('header');
+    element.classList.add('header');
+
+    const div = document.createElement('div');
+    div.classList.add('header-nav');
+
+    let btn = document.createElement('button');
+    btn.classList.add('header-btn-one');
+    btn.textContent = 'Home';
+    div.appendChild(btn);
+    btn = document.createElement('button');
+    btn.classList.add('header-btn-two');
+    btn.textContent = 'Menu';
+    div.appendChild(btn);
+    btn = document.createElement('button');
+    btn.classList.add('header-btn-three');
+    btn.textContent = 'Contact';
+    div.appendChild(btn);
+
+    element.appendChild(div);
+    
+    return element;
+};
+
+const content = document.querySelector('#content')
+
+content.appendChild(header());
+
+function homePage() {
+    const element = document.createElement('div');
+    element.classList.add('home-page');
+
+    return element;
+};
+
+content.appendChild(homePage());
+
+function menuPage() {
+    const element = document.createElement('div');
+    element.classList.add('menu-page','hidden');
+
+    return element;
+};
+
+content.appendChild(menuPage());
+
+function contactPage() {
+    const element = document.createElement('div');
+    element.classList.add('contact-page','hidden');
+
+    return element;
+};
+
+content.appendChild(contactPage());
 
 
 
-    btn.innerHTML = "click me";
+
+let btn1 = document.querySelector('.header-btn-one');
+let btn2 = document.querySelector('.header-btn-two');
+let btn3 = document.querySelector('.header-btn.three');
+const main = document.querySelector('.home-page');
+
+btn1.addEventListener('click', () => {
+    main.classList.remove('hidden');
+});
+
+btn2.addEventListener('click', () => {
+    main.classList.add('hidden');
+});
+
+btn3.addEventListener('click', () => {
+    main.classList.add('hidden');
+});
 
 
-    element.appendChild(btn)
 
-    return element
-}
-  
-document.body.appendChild(component());
 
--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ module.exports = {
     mode: 'development',
     entry: {
         index: './src/index.js',
+        style: './src/style.css',
     },
     devtool: 'inline-source-map',
     devServer: {
@@ -19,6 +20,18 @@ module.exports = {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
         clean: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+        ],
     },
     optimization: {
         runtimeChunk: 'single',
