3119eef48dcc936c962a6044a818d2185a6017e7
Mike Melchior <mike.melchior@outlook.com>
2022-06-17 13:27:54 -0400

    create basic menu content

--- src/menu.js
+++ src/menu.js
@@ -1,6 +1,34 @@
+import egg from './egg.png';
+
+const menuCard = function(text, price) {
+    const element = document.createElement('div');
+    element.classList.add('menu-card');
+
+    const p = document.createElement('p');
+    p.textContent = text;
+
+    const cost = document.createElement('p');
+    cost.classList.add('price');
+    cost.textContent= price;
+    
+    element.appendChild(p);
+    element.appendChild(cost);
+    return element;
+};
+
+
 export const menuPage = (function() {
     const element = document.createElement('div');
     element.classList.add('main-content','menu');
+
+    element.appendChild(menuCard('Eggs and Bacon', '$7.99'));
+    element.appendChild(menuCard("Grandma's Eggs", '$5.50'));
+    element.appendChild(menuCard('Burly Man Breakfast', '$9.50$'));
+    element.appendChild(menuCard('Breakfast Burrito', '$4.99'));
+    element.appendChild(menuCard('Omelette', '$5.99'));
+    element.appendChild(menuCard('Omelette', '$5.99'));
+
+
 
     return {
         element: element,
