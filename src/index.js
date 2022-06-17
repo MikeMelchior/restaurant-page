03dfbc2b5175c89ea1bf8c96cb3e7f4977b0fab6
Mike Melchior <mike.melchior@outlook.com>
2022-06-17 13:27:33 -0400

    add click events to nav buttons to load pages

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { homePage } from './homePage';
 import { menuPage } from './menu';
 import { contactPage } from './contact';
 import { footer } from './footer';
-import egg1 from './egg1.png'
+// import egg1 from './egg1.png'
 
 document.body.appendChild(header.element);
 
@@ -25,7 +25,21 @@ const main = document.querySelector('#content');
 main.appendChild(homePage.element);
 
 const homeBtn = document.querySelector('.header-btn-one');
+const menuBtn = document.querySelector('.header-btn-two');
+
 homeBtn.addEventListener('click', (e) => {
-    homeBtn.classList.toggle('active-btn')
-})
+    homeBtn.classList.add('active-btn');
+    menuBtn.classList.remove('active-btn');
+    main.appendChild(homePage.element);
+    main.removeChild(menuPage.element);
+
+});
+
+
+menuBtn.addEventListener('click', () => {
+    menuBtn.classList.add('active-btn');
+    homeBtn.classList.remove('active-btn');
+    main.appendChild(menuPage.element);
+    main.removeChild(homePage.element);
+});
 
